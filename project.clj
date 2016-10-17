77e40c2e2d211ea52d7ac69fee0e8b0e875612f6
David Orme <dorme@bradsdeals.com>
2016-10-17 15:50:03 -0500

    Bumped project version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.shopsmart/clj-infrastructure "0.1.4"
+(defproject com.github.shopsmart/clj-infrastructure "0.1.5"
   :description "Infrastructure helpers for AWS, database, etc."
   :url "https://github.com/shopsmart/clj-foundation"
 
