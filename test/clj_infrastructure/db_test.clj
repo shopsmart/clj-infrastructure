379608a9a2f1c038bd410f9e03e4421ffea07d77
Levi Dixon <levi@bradsdeals.com>
2016-10-24 13:01:33 -0500

    Simplify serializable transaction error test and change tests to use new redshift user "integration_or_unit_test"

--- test/clj_infrastructure/db_test.clj
+++ test/clj_infrastructure/db_test.clj
@@ -45,7 +45,7 @@
 
 (def settings {:redshift {:test-table "integration_or_unit_test.transaction_test_777"
                           :test-table-2 "integration_or_unit_test.transaction_test_999"
-                          :spec (secrets :contentshift)}
+                          :spec (secrets :integration_or_unit_test)}
 
                :h2 {:test-table "transaction_test_777"
                     :test-table-2 "transaction_test_999"
@@ -69,7 +69,7 @@
    (execute! "drop table if exists ${test-table-name};"
              CONNECTION conn
              :test-table-name table-name)
-   (execute! "create table if not exists ${test-table-name} (id bigint, description varchar(1000), id2 bigint identity);"
+   (execute! "create table if not exists ${test-table-name} (id bigint, description varchar(1000), id_seq bigint identity);"
              CONNECTION conn
              :test-table-name table-name))
   ([conn]
@@ -607,48 +607,38 @@
     (create-test-table trans1 (config :test-table))
     (j-update CONNECTION trans1 :table (config :test-table) :values "blah = blah" :where "invalid sql")))
 
+
 (deftest serializable-transaction-behavior
+  "NOTE: The addition of the column \"id_seq bigint identity\" to the table accompany this test
+  (to force a system catalog table update (with the auto-generated sequence value) when a table
+  row is inserted.  It is unclear whether this is causal to the issue yet."
 
   (clojure.pprint/pprint (config DB-SPEC))
 
-  (is (thrown-with-msg? org.postgresql.util.PSQLException #"Detail: Serializable isolation violation"
+  (testing "Multiple transactions can violate serializable transaciton integrity; causing the violating transaction to cancel, rollback, and throw an exception."
 
-    (testing "Multiple transactions can write to the same table without violating serializable transaciton integrity"
+    (is (thrown-with-msg? org.postgresql.util.PSQLException #"Detail: Serializable isolation violation"
 
-      ;; Setup/initialization
       (jdbc/with-db-transaction [trans3 (config DB-SPEC)]
 
-      (jdbc/with-db-transaction [trans1 (config DB-SPEC)]
-        (create-test-table trans1 (config :test-table))
-        (create-test-table trans1 "integration_or_unit_test.transaction_test_900")
-        (create-test-table trans1 "integration_or_unit_test.transaction_test_901")
-        (create-test-table trans1 "integration_or_unit_test.transaction_test_903"))
+        ;; Setup/initialization
+        (jdbc/with-db-transaction [trans1 (config DB-SPEC)]
+          (create-test-table trans1 (config :test-table))
+          (create-test-table trans1 "integration_or_unit_test.transaction_test_900")
+          (create-test-table trans1 "integration_or_unit_test.transaction_test_901"))
 
-     (j-select CONNECTION trans3 :table "integration_or_unit_test.transaction_test_901" :where "")
+          ; Ensure transaciton3 needs a snapshot of the table at this point in time
+          (j-select CONNECTION trans3 :table "integration_or_unit_test.transaction_test_901" :where "")
 
-      (jdbc/with-db-transaction [trans1 (config DB-SPEC)]
-        (j-insert CONNECTION trans1 :table (config :test-table) :values "(2, 'Row 1 - Trans 1')")
-        (j-insert CONNECTION trans1 :table "integration_or_unit_test.transaction_test_900" :values "(2, 'Row 1 - Trans 1')")
-        (j-insert CONNECTION trans1 :table "integration_or_unit_test.transaction_test_901" :values "(2, 'Row 1 - Trans 1')"))
+          (jdbc/with-db-transaction [trans2 (config DB-SPEC)]
+            (j-update   CONNECTION trans2 :table "integration_or_unit_test.transaction_test_900" :values "id = 1" :where "id = 2")
+            (j-truncate CONNECTION trans2 :table "integration_or_unit_test.transaction_test_900"))
+            ;(j-delete   CONNECTION trans2 :table (config :test-table) :where  "id = 3"))
 
-        (jdbc/with-db-transaction [trans2 (config DB-SPEC)]
+          (jdbc/with-db-transaction [trans1 (config DB-SPEC)]
+            (j-delete CONNECTION trans1 :table (config :test-table) :where "id >= 10")
+            (j-delete CONNECTION trans3 :table "integration_or_unit_test.transaction_test_900" :where "id >= 0")))))))
 
-          (j-update CONNECTION trans2 :table "integration_or_unit_test.transaction_test_900" :values "id = 1" :where "id = 2")
-          (j-update CONNECTION trans2 :table "integration_or_unit_test.transaction_test_901" :values "id = 2" :where "id = 1")
-          (j-truncate CONNECTION trans2 :table (config :test-table))
-          (j-truncate CONNECTION trans2 :table "integration_or_unit_test.transaction_test_900")
-
-            (j-insert CONNECTION trans2 :table (config :test-table) :values "(3, 'Row 3 - Trans 2')")
-            (j-delete CONNECTION trans2 :table (config :test-table) :where  "id = 3"))
-
-      (jdbc/with-db-transaction [trans1 (config DB-SPEC)]
-        (j-insert CONNECTION trans1 :table (config :test-table) :values "(2, 'Row 2 - Trans 1')")
-
-        (j-delete CONNECTION trans1 :table (config :test-table) :where "id >= 10")
-
-        (testing "Update" (j-update CONNECTION trans1 :table (config :test-table) :values "description = 'Updated value: Row 2 - Trans 1'" :where "id = 3"))
-
-        (testing "delete" (j-delete CONNECTION trans3 :table "integration_or_unit_test.transaction_test_900" :where "id >= 0"))))))))
 
 (run-tests)
 
