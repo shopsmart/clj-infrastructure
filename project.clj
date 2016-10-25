10d6631a2cbdf924df9edafeae832d6bfe8e9ce6
David Orme <dorme@bradsdeals.com>
2016-10-25 11:03:26 -0500

    Bump project version to pick up new dependencies

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.shopsmart/clj-infrastructure "0.1.8"
+(defproject com.github.shopsmart/clj-infrastructure "0.1.9"
   :description "Infrastructure helpers for AWS, database, etc."
   :url "https://github.com/shopsmart/clj-infrastructure"
 
